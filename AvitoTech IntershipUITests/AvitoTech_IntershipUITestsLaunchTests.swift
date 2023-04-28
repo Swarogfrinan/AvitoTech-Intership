05e84fa63ca1a72027d6fdc5c8b858ee32e374e6
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2023-04-28 13:52:50 +0300

    Initial Commit

---
+++ AvitoTech IntershipUITests/AvitoTech_IntershipUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AvitoTech_IntershipUITestsLaunchTests.swift
+//  AvitoTech IntershipUITests
+//
+//  Created by Ilya Vasilev on 28.04.2023.
+//
+
+import XCTest
+
+final class AvitoTech_IntershipUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
