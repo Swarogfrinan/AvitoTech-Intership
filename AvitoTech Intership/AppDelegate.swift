6b2791da48ec460e74bac8f2c2585fb493d442cc
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2023-05-08 13:24:54 +0300

    removed empty placed

--- AvitoTech Intership/AppDelegate.swift
+++ AvitoTech Intership/AppDelegate.swift
@@ -1,19 +1,9 @@
-//
-//  AppDelegate.swift
-//  AvitoTech Intership
-//
-//  Created by Ilya Vasilev on 28.04.2023.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
 
