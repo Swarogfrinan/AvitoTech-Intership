950a5a6a494cd081a43899b5821fdc228184a684
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2023-05-08 13:25:09 +0300

    Create MainContainerViewModel.swift

---
+++ AvitoTech Intership/ViewModel/MainContainerViewModel.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+class MainContainerVM {
+    private var data: PageData!
+    var selectedSuggestion: SuggestionCellVM?
+    
+    var title: String  {
+        return data.title
+    }
+    var actionTitle: String {
+        return data.actionTitle
+    }
+    
+    var selectedActionTitle: String {
+        return data.selectedActionTitle
+    }
+    
+    init() {
+        Service.instance.loadData(completion: { (data) in
+            self.data = data
+        })
+    }
+    
+    func viewModelForSuggestionList() -> SuggestionListVM {
+        return SuggestionListVM(data.list)
+    }
+    
+}
