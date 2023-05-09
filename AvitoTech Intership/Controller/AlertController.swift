dc2c71130806cdab69a444467498f138a4c636bf
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2023-05-09 16:44:23 +0300

    Create AlertController.swift

---
+++ AvitoTech Intership/Controller/AlertController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class AlertController: UIAlertController {
+    //MARK: - Properties
+    var viewModel: SuggestionCellVM? {
+        didSet {
+            configureUI()
+        }
+    }
+    //MARK: - Helpers
+    func configureUI() {
+        addAction(UIAlertAction(title: "Закрыть", style: .cancel))
+        guard let viewModel = viewModel else {
+            title = "Необходимо выбрать один из вариантов"
+            return
+        }
+        title = "\(viewModel.title)"
+        message = "\(viewModel.description)"
+    }
+}
