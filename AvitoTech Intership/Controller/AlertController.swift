d656bc0d5532a5c831b9795515c920351f0e6bc8
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2023-05-20 20:25:18 +0300

    Update AlertController to Constants

--- AvitoTech Intership/Controller/AlertController.swift
+++ AvitoTech Intership/Controller/AlertController.swift
@@ -1,21 +1,27 @@
 import UIKit
 
 class AlertController: UIAlertController {
-    //MARK: - Properties
     
+    //MARK: - Properties
     var viewModel: SuggestionCellVM? {
         didSet {
             configureUI()
         }
     }
+    
     //MARK: - Helpers
     func configureUI() {
-        addAction(UIAlertAction(title: "Закрыть", style: .cancel))
+        addAction(UIAlertAction(title: Constants.title, style: .cancel))
         guard let viewModel = viewModel else {
-            title = "Необходимо выбрать один из вариантов"
+            title = Constants.message
             return
         }
         title = "\(viewModel.title)"
         message = "\(viewModel.description)"
     }
+}
+
+private extension Constants {
+    static let title = "Закрыть"
+    static let message = "Необходимо выбрать один из вариантов"
 }
