f5859b8c02a58131b9357a90ed9d68fa967dd8e3
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2023-05-20 20:22:36 +0300

    Crate Constants file

---
+++ Supported/Constant.swift
@@ -0,0 +1,10 @@
+import Foundation
+enum Constants {
+    static let spacing : CGFloat = 20
+    static let indent : CGFloat = 15
+    static let defaultHeight : CGFloat = 50
+    static let cornerRadius : CGFloat = 5
+    static let bottom : CGFloat = 25
+    static let fontOfScreenTitle : CGFloat = 30
+    static let top : CGFloat = 30
+}
