223b9799c1085db1b1beb16ce3e4440ea5f631b3
Swarogfrinan <Ilyavasilev.wow@gmail.com>
2023-05-20 21:08:50 +0300

    Update Constant.swift

--- Supported/Constant.swift
+++ Supported/Constant.swift
@@ -1,9 +1,12 @@
 import Foundation
+
+// MARK: - Constants
 enum Constants {
     static let spacing : CGFloat = 20
     static let indent : CGFloat = 15
     static let defaultHeight : CGFloat = 50
     static let cornerRadius : CGFloat = 5
+    static let cellCornerRadius : CGFloat = 10
     static let bottom : CGFloat = 25
     static let fontOfScreenTitle : CGFloat = 30
     static let top : CGFloat = 30
